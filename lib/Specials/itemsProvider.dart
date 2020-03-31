bd9aba77ce657ba20f3d56fa512e4cb6600a5957
20bvsa56 <bivisha2056@gmail.com>
2020-03-31 13:18:53 +0545

    provider class and bloc class is made

---
+++ lib/Specials/itemsProvider.dart
@@ -0,0 +1,19 @@
+import 'items.dart';
+
+//class created to create a list of food items to be updated and later fed into sink
+
+class BeverageCartProvider{
+  List <Items> specialitems =[]; //creating list of SpecialFoodItem and initializing with empty list.
+  
+  //now adding received food items to the list initalized above.
+  List<Items> addToList(Items items){ 
+    specialitems.add(items);
+    return specialitems; // return newly updated list
+  }
+
+  //now removing food items from the list.
+  List<Items> removeFromList(Items items){
+    specialitems.remove(items);
+    return specialitems;// return newly updated list
+  }
+} 
